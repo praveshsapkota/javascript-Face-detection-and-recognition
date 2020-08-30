71ae6d4ed52d38bc43ea6d03733044c5e7dfa917
Ethan_rogue <praveshsaptoka2@gmail.com>
2020-08-30 08:58:25 -0400

    2.0

--- backend.js
+++ backend.js
@@ -1,78 +1,174 @@
-//const cv = require('opencv4nodejs');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const parser = require('body-parser');
-const fs  = require('fs');
+const fs = require('fs');
+const mysql = require('mysql')
+const date = require('date-and-time');
 
 
-app.use('/static', express.static(__dirname+'/public'))
-app.use(parser.urlencoded({extended:'true'}));
+app.set('view engine', 'ejs');
+app.use('/static', express.static(__dirname + '/public'))
+app.use(parser.urlencoded({
+    extended: 'true'
+}));
 
 
-app.get('/',function(req,res){res.sendFile(__dirname+'/HTML/index.html')})
-app.get('/register',function(req,res){res.sendFile(__dirname+"/HTML/Register_User.html")});
-app.get('/recognition',(req,res)=>{
-    res.sendFile(__dirname+"/HTML/Face_Recognition.html")
-    create_labels();
+var db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    port: '3306',
+    database: 'db'
 });
 
-var name = 0;
-var department = 0;
-var imgdata = [];
-var  labels = []; 
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('database connected');
+    }
+});
 
 
-function create_labels(){
-    for(i=0;i<fs.readdirSync(__dirname+'/public/images/').length;i++){
-        var user_name = fs.readdirSync(__dirname+'/public/images/')[i].split('.')[0]
+app.get('/', function (req, res) {
+    res.render('index')
+})
+app.get('/register', function (req, res) {
+    res.render("Register_User")
+});
+app.get('/recognition', (req, res) => {
+    let query = ('DELETE FROM Attendance');
+    fetch_data(query, (results) => {});
+    create_labels();
+    res.render('Face_Recognition');
+
+});
+app.get('/attendance', (req, res) => {
+    let query = 'SELECT t1.*,t2.Date FROM register t1,Attendance t2 WHERE t1.USN = t2.USN'
+    fetch_data(query, (result) => {
+        res.json(result);
+        console.log(result)
+    });
+});
+
+app.get('/index2', (req, res) => {
+    let query = 'SELECT * FROM register'
+    fetch_data(query, (results) => {
+        res.render('tabel_data', {
+            rows: results
+        })
+    });
+})
+
+
+app.get('/registered', (req, res) => {
+    let query = 'SELECT * FROM register'
+    fetch_data(query, (results) => {
+        res.render('tabel_data', {
+            rows: results
+        })
+    });
+});
+
+
+function fetch_data(query, cb) {
+    db.query(query, (err, result, fields) => {
+        if (err) {
+            console.error(err.sqlMessage);
+            return;
+        }
+        cb(result)
+    })
+
+}
+
+
+var labels = [];
+
+function create_labels() {
+    labels = [];
+    for (i = 0; i < fs.readdirSync(__dirname + '/public/images/').length; i++) {
+        var user_name = fs.readdirSync(__dirname + '/public/images/')[i].split('.')[0]
         labels.push(user_name)
     }
 }
 
 io.on('connect', socket => {
     var j = 0;
-    socket.on('imgdata', (data) => {
-      for(j=0;j<data.length;j++){
-        imgdata.push(data[j])  
-      }
-      createuser(name,department,imgdata);
+    socket.on('userdata', (data) => {
+        var name = data.name;
+        var department = data.dep;
+        var semester = data.sem;
+        var usn = data.usn;
+        var imgdata = data.choosed_image.img;
+        console.log(name, department, semester, usn)
+        if(name != undefined || usn != undefined || department != undefined || semester != undefined|| imgdata != undefined){
+            query = `INSERT INTO register VALUES('${usn}','${name}','${department}','${semester}')`
+        db.query(query, (err, response) => {
+            if (err) {
+                console.error(err.sqlMessage);
+                return;
+            }
+            console.log(response.insertId);
+        });
+        createuser(name, usn, imgdata);
+        }
+        else{
+            console.log('Please submitt full information in order of register');
+        }
+        
+
     });
-    socket.emit('labels',labels);
+    socket.emit('labels', labels);
 });
 
-app.post('/submitt_data',(req,res)=>{
-    res.send('new user uploaded');
-    name = req.body.name;
-    department = req.body.department
-    console.log(name,department) 
-})
 
-function createuser(name,department,imgdata){
-    if(fs.existsSync(__dirname+'/images'+'/'+name+'_'+department+'.jpg')){
-        console.log('user named '+name+" already exists");
-    }
-    else{
-        var i = 0;
-        for(i=0;i<imgdata.length;i++){
-            var data = imgdata[i]
-            var base64Data = data.replace(/^data:image\/png;base64,/, "");
-            fs.writeFile(__dirname+'/public/images/'+name+'_'+department+[i]+".png", base64Data, 'base64', 
-            function(err, data) {
+function createuser(name, usn, imgdata) {
+    if (fs.existsSync(__dirname + '/public/images' + '/' + name + '_' + usn + '.png')) {
+        console.log('user named ' + name + usn + " already exists");
+    } else {
+        if(imgdata != undefined){
+        var data = imgdata
+        var base64Data = data.replace(/^data:image\/png;base64,/, "");
+        fs.writeFile(__dirname + '/public/images/' + name + '_' + usn + ".png", base64Data, 'base64',
+            function (err, data) {
                 if (err) {
                     console.log('err', err);
                 }
                 console.log('success');
-
             });
-
-        };
+        }
+        else{
+            console.log('no image selected');
+        }
     }
 }
 
 
+setTimeout(function () {
+    var recognized = 0;
+    io.on('connect', (socket) => {
+        socket.on('recognized', (data) => {
+            recognized = [data.split('_')[1], date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')]
+            if (recognized[0] != undefined) {
+                console.log(recognized)
+                query = `INSERT INTO Attendance VALUES('${recognized[0]}','${recognized[1]}')`
+                db.query(query, (err, response) => {
+                    if (err) {
+                        console.error(err.sqlMessage);
+                        return;
+                    }
+                    console.log(response.insertId);
+                });
+            }
 
-server.listen(5000,function(){
+        });
+    });
+}, 500)
+
+
+server.listen(5000, function () {
     console.log('server started at port 5000')
 });
